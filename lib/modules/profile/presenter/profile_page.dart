3e321125548cee042f977d281320e0530a050b99
chornthoen <thoenchorn@gmail.com>
2024-02-11 14:21:38 +0700

    Update profile

--- lib/modules/profile/presenter/profile_page.dart
+++ lib/modules/profile/presenter/profile_page.dart
@@ -5,6 +5,13 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Text('Profile Page'),
+      ),
+    );
   }
 }
